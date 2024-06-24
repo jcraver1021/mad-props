26dab4e185740391c06ea9b25eb33af1dced9389
James Craver <25520616+jcraver1021@users.noreply.github.com>
2024-06-23 21:06:35 -0400

    format

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
 import "./App.css";
 
 function App() {
-
   return (
     <>
       <h1>Half-baked experiments</h1>
       <div className="card">
-        <p>
-          Under construction
-        </p>
+        <p>Under construction</p>
       </div>
     </>
   );
