f539acf5e5801244bdb9aa5c781414d824d262aa
James Craver <25520616+jcraver1021@users.noreply.github.com>
2024-07-08 02:16:03 +0000

    Adds routing and app card (#7)

---
+++ src/pages/home.tsx
@@ -0,0 +1,14 @@
+import AppCard from "../components/AppCard/AppCard";
+
+function Home() {
+  return (
+    <>
+      <h1>Half-baked experiments</h1>
+      <div>
+        <AppCard name="Example" to="/example" />
+      </div>
+    </>
+  );
+}
+
+export default Home;
