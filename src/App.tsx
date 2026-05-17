6ddac773fad07f986fb0df2ad9141ccec16e936a
James Craver <25520616+jcraver1021@users.noreply.github.com>
2026-05-16 20:30:17 -0400

    Adds main theme (#10)
    * Adds main theme
    
    * it'd be nice to actually add the new file

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,22 @@
 import "./App.css";
+import { ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Example from "./pages/example";
 import Home from "./pages/home";
+import { theme } from "./theme";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/example" element={<Example />} />
-      </Routes>
-    </BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/example" element={<Example />} />
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
 
